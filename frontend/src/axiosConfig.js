c5cfee8e23f616caa51f2fb0da78b705c9fd7948
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2024-04-17 08:56:54 -0400

    deleted:    frontend/project 	modified:   frontend/src/axiosConfig.js 	modified:   frontend/src/components/Carousel/Carousel.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '/';
+    process.env.NODE_ENV === 'development' ? 'https://cartierkuti.onrender.com' : '/';
