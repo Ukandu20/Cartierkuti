6edeadb32c83744b24955ea9cbc3d5bb07b3a8bc
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2024-04-17 13:01:28 -0400

    new file:   backend/src/routers/router.js 	modified:   backend/src/server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import projectRouter from './routers/project.router.js';
+import router from './routers/router.js';
 
 
 const app = express();
 app.use(cors({
-    origin: ['https://main--cartierkuti.netlify.app/'],
+    origin: ['http://localhost:3002'],
     credentials: true
 }));
 
 app.use('/api/projects', projectRouter);
 
-const PORT = 5001;
+app.get('/', router);
+
+
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ` + PORT);
 });
