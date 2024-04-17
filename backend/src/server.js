f3923b2adcbeaa1c7176ead295f0146e1b01f3c6
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2024-04-17 09:21:05 -0400

    modified:   backend/src/server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import projectRouter from './routers/project.router.js';
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3002'],
     credentials: true
 }));
 
