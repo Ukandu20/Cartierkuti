08b262b380eb1a5678408638bb4b3f6452b6e995
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2024-04-24 01:05:50 -0400

    modified:   frontend/src/axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,4 +2,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://cartierkuti.onrender.com/';
+    process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'http://cartierkuti.onrender.com/';
